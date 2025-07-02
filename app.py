4e116b4fff9b7db0424520519f662fca40dee7f8
Abbas <tundeeey@gmail.com>
2025-07-02 18:15:34 +0100

    creating the add function

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template, url_for
-from flask_scss import Scss
-
+import json
+from flask import Flask, render_template, url_for, redirect, request
+from datetime import datetime
 
 app = Flask(__name__)
+
+def load_data():
+    try:
+        # loading data from json
+        with open('data.json', 'r') as file:
+            todo = json.load(file)
+            return todo
+
+    except FileNotFoundError:
+        print("No saved data found")
+
+
+# function to save data into json
+def save_data(todo):
+    # Writing to json
+    with open("data.json", "w") as file:
+        json.dump(todo, file, indent=4)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    todo = load_data()
+    return render_template("index.html", todo=todo)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+    date = datetime.now().strftime("%Y-%m-%d")
+    todos = load_data()
+    todos.append({
+        'id': str(uuid.uuid4()),  # Unique identifier
+        'task': task,
+        'date': date
+    })
+    save_data(todos)
+    return redirect('/')
 
 
 
